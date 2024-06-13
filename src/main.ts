ca2e1c4ed98b5f4677909282f60e4acdc3d15e69
David69699 <abutidzedato18@gmail.com>
2024-06-13 09:27:43 +0400

    done

---
+++ src/main.ts
@@ -0,0 +1,19 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { SafeGuard } from './users/users.guard';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
+
+async function bootstrap() {
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.useGlobalGuards(new SafeGuard());
+  app.useGlobalPipes(new ValidationPipe());
+
+  app.useStaticAssets(join(__dirname, '..', 'public'));
+  app.setBaseViewsDir(join(__dirname, '..', 'views'));
+  app.setViewEngine('ejs');
+
+  await app.listen(3000);
+}
+bootstrap();
---
+++ src/users/users.controller.ts
@@ -0,0 +1,64 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  UseGuards,
+  Render,
+} from '@nestjs/common';
+import { UsersService } from './users.service';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { UserIDValidateDTO } from './dto/user-id.dto';
+import { AuthGuard, IsAdmin } from './users.guard';
+
+@Controller('users')
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Get()
+  @Render('homepage')
+  async root() {
+    const users = await this.usersService.findAll();
+    return { users: users };
+  }
+
+  @UseGuards(IsAdmin)
+  @Post()
+  create(@Body() createUserDto: CreateUserDto) {
+    return this.usersService.create(createUserDto);
+  }
+
+  @UseGuards(AuthGuard)
+  @Get()
+  findAll() {
+    return this.usersService.findAll();
+  }
+
+  @UseGuards(IsAdmin)
+  @Get(':id')
+  findOne(@Param() param: UserIDValidateDTO) {
+    const { id } = param;
+    return this.usersService.findOne(id);
+  }
+
+  @UseGuards(IsAdmin)
+  @Patch(':id')
+  update(
+    @Param() param: UserIDValidateDTO,
+    @Body() updateUserDto: UpdateUserDto,
+  ) {
+    const { id } = param;
+    return this.usersService.update(id, updateUserDto);
+  }
+
+  @UseGuards(IsAdmin)
+  @Delete(':id')
+  remove(@Param() param: UserIDValidateDTO) {
+    const { id } = param;
+    return this.usersService.remove(id);
+  }
+}
